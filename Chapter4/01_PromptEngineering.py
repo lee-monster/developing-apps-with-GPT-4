ff3a1a18782cd0b19a3eacb913b7c9c45bddd0ab
lee-monster <leeilsup0909@gmail.com>
2023-12-28 00:32:06 +0900

    Update 01_PromptEngineering.py

--- Chapter4/01_PromptEngineering.py
+++ Chapter4/01_PromptEngineering.py
@@ -10,3 +10,25 @@ print(message)
 
 
 chat_completion("As Descartes said, I think therefore")
+
+
+##Context, task and role
+
+chat_completion("Give me a suggestion for the main course for today's lunch.")
+
+prompt = """
+Context: I do 2 hours of sport a day. I am vegetarian, and I don't like green
+vegetables. I am conscientious about eating healthily.
+Task: Give me a suggestion for a main course for today's lunch.
+"""
+chat_completion(prompt)
+
+
+prompt = """
+Context: I do 2 hours of sport a day. I am vegetarian and I don't like green
+vegetables. I am very careful to eat healthily.
+Task: Give me a suggestion for a main course for today's lunch?
+Do not perform the requested task! Instead, can you ask me questions about the context
+so that when I answer, you can perform the requested task more efficiently?
+"""
+chat_completion(prompt)
