fa8d458e58c838518e74b36c13d4f5331bde4213
lee-monster <leeilsup0909@gmail.com>
2023-12-28 00:30:22 +0900

    Create 01_PromptEngineering.py

---
+++ Chapter4/01_PromptEngineering.py
@@ -0,0 +1,9 @@
+def chat_completion(prompt, model="gpt-4-1106-preview", temperature=0):
+  res = client.chat.completions.create(
+    model=model,
+    messages=[{"role": "user", "content": prompt}],
+    temperature=temperature,
+)
+
+message = res.choices[0].message.content
+print(message)
